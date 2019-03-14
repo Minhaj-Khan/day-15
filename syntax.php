49483ec69688e0160283091d0e181ed3f203a10c
Minhaj-Khan <minhaj.mehdi.khan@gmail.com>
2019-03-14 07:43:14 +0600

    PHP introduction and basics

---
+++ syntax.php
@@ -0,0 +1,116 @@
+<?php
+
+//echo "Hello World";
+
+//echo "<html>";
+//    echo "<head>";
+//        echo "<title>";
+//                echo "Home";
+//        echo "</title>";
+//    echo "</head>";
+//        echo "<body>";
+//        echo "<h1>This is a heading.</h1>";
+//        echo "<p>This is a paragraph.</p>";
+//        echo "</body>";
+//echo "</html>";
+?>
+<!--<html>-->
+<!--<head>-->
+<!--<meta charset="UTF-8"/>-->
+<!--    <title>--><?php //echo "Home"?><!--</title>-->
+<!--</head>-->
+<!--<body>-->
+<!--<h1>--><?php //echo "This is a heading."?><!--</h1>-->
+<!--<p>--><?php //echo "This is a paragraph."?><!--</p>-->
+<!--<input type="text" value="--><?php //echo "Hello World"; ?><!--"/>-->
+<!--</body>-->
+<!--</html>-->
+<?php
+
+//3 Standard Rules for declaring a variable
+/*
+ * *start with $ sign
+ * A-Z, a-z, 0-9,_
+ * No Number in first
+ */
+/*3 standaard rules for declaring a vriable
+*small letter
+ * meaning full
+ * readable
+*/
+//$NAME;
+//$name;
+//$_9name;
+//$name;
+//$Name;
+//
+//$name="Minhaj";
+//$number=10;
+//$firstName="Minhaj"; //Standard Rules
+//$lastName="Khan";
+////echo $firstName;
+//echo $firstName." ".$lastName;
+
+//    $name="Minhaj";
+       /*
+        * Arithmatic Operator
+        * Binary(+,-,*,/,%)
+        * Unary(++,--;(-))
+        * Assignment Operator
+        * (=,+=,-=,*=,/=,%=,.=)
+        * Conditional Operator
+        * (>,>=,<,<=,==,!=,===,!==)
+        * Logical Operator
+        * &&,||,!
+        */
+       $x=10;
+       $y=20;
+////       echo $x+$y;
+//        echo ++$x; //11
+//        echo "<br>";
+//        echo $x--; //11
+//        echo "<br>";
+//        echo $x--; //10
+//        echo "<br>";
+//        echo --$x;  //8
+
+//            echo $x+=$y;  //30\
+//            echo  "<br>";
+//            echo $x-=$y; //10
+//            echo  "<br>";
+//            echo $x.=$y; //1020
+
+            //Statements
+            /*
+             * *single Line Statements
+             * *Conditional Statements
+             * if
+             * if else
+             * if else if
+             * switch
+             * Repeated Statements
+             * for
+             * while
+             * do while
+             * foreach
+             */
+
+            $x=10;
+            $y=20;
+//            if($x>$y)
+//            {
+//            $z=$x+$y;
+//            echo $z;
+//            }
+
+//                if($x<$y)
+//                {
+//                    $z=$x+$y;
+//                    echo $z;
+//                }
+
+
+
+
+
+?>
